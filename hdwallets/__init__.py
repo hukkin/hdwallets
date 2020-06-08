3d4b1fe6b36ce5ce11085c3df573e912e8ae758f
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-06-08 16:48:11 +0300

    Add bumpversion conf

--- hdwallets/__init__.py
+++ hdwallets/__init__.py
@@ -1,7 +1,7 @@
 from .bip32 import BIP32
 from .utils import BIP32DerivationError, HARDENED_INDEX
 
-__version__ = "0.0.9"
+__version__ = "0.0.9"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 __all__ = [
     "BIP32",
