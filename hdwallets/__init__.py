98a5f84dc0a18abe6cf2a477683a166094107040
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-08-31 13:52:01 +0300

    Bump version: 0.1.0 → 0.1.1

--- hdwallets/__init__.py
+++ hdwallets/__init__.py
@@ -1,7 +1,7 @@
 from ._bip32 import BIP32
 from ._utils import HARDENED_INDEX, BIP32DerivationError
 
-__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 __all__ = (
     "BIP32",
